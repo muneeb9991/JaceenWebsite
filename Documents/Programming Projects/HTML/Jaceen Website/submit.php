3ca9f8456f2bb091ef7ca37c29d39c8f21e25d26
muneeb9991 <135388596+muneeb9991@users.noreply.github.com>
2024-01-09 12:58:53 +0000

    form change php

--- Documents/Programming Projects/HTML/Jaceen Website/submit.php
+++ Documents/Programming Projects/HTML/Jaceen Website/submit.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection
-$conn = new mysqli("localhost", "root", "", "form_data");
+$conn = new mysqli("localhost", "root", "", "jaceen_contact_form_data");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
